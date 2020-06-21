b57e64699d65cde294ffe22c10af45f6393f2279
brettmichaelgreen <me@brettmichaelgreen.com>
2020-06-21 12:06:42 -0400

    add env variables

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
@@ -7,4 +10,8 @@ def hello():
     return render_template("whale_hello.html")
 
 if __name__ == '__main__':
+    project_folder = os.path.dirname(os.path.abspath(__file__))
+    load_dotenv(os.path.join(project_folder, '.env'))
+    TDAMERITRADE_CLIENT_ID = os.getenv("TDAMERITRADE_CLIENT_ID")
+    print(TDAMERITRADE_CLIENT_ID)
     app.run(debug=True, host='0.0.0.0')
