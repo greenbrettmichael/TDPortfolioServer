4fbe45b50b1140f2576cec13ea8f15921bc874f0
brettmichaelgreen <me@brettmichaelgreen.com>
2020-06-21 16:14:56 -0400

    dump docker since selenium is difficult there

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7-alpine
-
-COPY requirements.txt /
-RUN pip install --no-cache-dir -r /requirements.txt
-COPY . /app
-WORKDIR /app
-
-ENTRYPOINT ["python"]
-CMD ["app.py"]
--- app.py
+++ app.py
@@ -9,30 +9,23 @@ app = Flask(__name__)
 def make_webdriver():
     from selenium import webdriver
     from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-    browser = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
+    
+    browser = webdriver.Chrome()
     atexit.register(lambda: browser.quit())
     return browser
 
 @app.route("/")
 def hello():
     tda.auth.easy_client(current_app.config["TDAMERITRADE_CLIENT_ID"], current_app.config["REDIRECT_URI"], current_app.config["PATH_TO_TOKEN"], make_webdriver)
-    #c = tda.auth.client_from_login_flow(browser, current_app.config["TDAMERITRADE_CLIENT_ID"], current_app.config["REDIRECT_URI"], current_app.config["PATH_TO_TOKEN"])
     return render_template("whale_hello.html")
 
 if __name__ == '__main__':
-    if getattr(sys, 'gettrace', None):
-        import multiprocessing
-        if multiprocessing.current_process().pid > 1:
-            import debugpy
-            print("ATTACH DEBUGGER TO 5678")
-            debugpy.listen(("0.0.0.0", 5678))
-            debugpy.wait_for_client()
     project_folder = os.path.dirname(os.path.abspath(__file__))
     load_dotenv(os.path.join(project_folder, '.env'))
+    authtoken_path = os.path.join(project_folder, 'authtoken.txt')
     app.config.update(
     TDAMERITRADE_CLIENT_ID=os.getenv("TDAMERITRADE_CLIENT_ID"),
     REDIRECT_URI=os.getenv("REDIRECT_URI"),
-    PATH_TO_TOKEN=os.getenv("PATH_TO_TOKEN")
+    PATH_TO_TOKEN=authtoken_path
     )
-    app.run(debug=False, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='5000', ssl_context='adhoc')
